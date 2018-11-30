72a71c1ed9e1d38115847411d5139fc093431a24
usuldib32 <usuldib32@gmail.com>
2018-11-30 17:43:01 +0100

    task 10 working

---
+++ src/main/java/zadania_java_level0/Zadanie10.java
@@ -0,0 +1,34 @@
+package zadania_java_level0;
+
+//add given numbers until user will press 0
+
+import java.util.Scanner;
+
+public class Zadanie10 {
+
+    static final float STOP_COUNT = 0;
+
+    public static void main(String[] args) {
+        System.out.println("program będzie sumował podane liczby póki użytkownik nie poda 0.\nWprowadź pierwszą wartość:");
+
+        Scanner rd = new Scanner(System.in);
+
+
+        float givenNumber = rd.nextFloat();
+        float sum = STOP_COUNT;    //if first given will be 0 than we got proper sum already
+
+        while (givenNumber != STOP_COUNT) {
+            System.out.println("wprowadź kolejną wartość");
+            sum += givenNumber;
+            givenNumber = rd.nextFloat();
+        }
+
+        System.out.println("suma wprowadzonych liczb wynosi " + sum);
+
+//        do {
+//            sum += givenNumber;
+//            givenNumber = rd.nextFloat();
+//            System.out.println("wprowadź kolejną wartość");
+//        } while (givenNumber != 0f);
+    }
+}
