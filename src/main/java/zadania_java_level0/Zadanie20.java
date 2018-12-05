53eb603d2e223f30de4cc716c5059d6c00eaa460
usuldib32 <usuldib32@gmail.com>
2018-12-05 18:12:16 +0100

    task 20 with 32bit word and output show format

--- src/main/java/zadania_java_level0/Zadanie20.java
+++ src/main/java/zadania_java_level0/Zadanie20.java
@@ -1,12 +1,18 @@
 package zadania_java_level0;
 
 //changes decimal numbers into binary
+//todo
+// :) as usual, obj
 
 import java.util.Scanner;
 
 public class Zadanie20 {
 
     final static int DIVIDER = 2;
+    final static int ZERO = 0;
+    final static int ONE = 1;
+    final static int WORD_LENGTH = 32;
+    final static int OUTPUT_SHOW_FORMAT = 4;
 
     public static void main(String[] args) {
         System.out.println("program zamienia wartości z kodu dzesiętnego na binarny");
@@ -16,35 +22,23 @@ public class Zadanie20 {
         int userNumber = rd.nextInt();
         rd.close();
 
-        int[] binaryInterpretArray = new int[4];
-//        for (int i = 0; i < binaryInterpretArray.length; i++) {
-//            binaryInterpretArray[i] = 0;
-//        }
+        int[] binaryInterpretArray = new int[WORD_LENGTH];
 
         for (int i = 0; i < binaryInterpretArray.length; i++) {
-            if (userNumber % DIVIDER == 0) {        //cheking if it divides completely, if yes, put 0 into binary array
+            if (userNumber % DIVIDER == ZERO) {        //cheking if it divides completely, if yes, put 0 into binary array
                 userNumber /= DIVIDER;
-                binaryInterpretArray[i] = 0;
+                binaryInterpretArray[i] = ZERO;
             } else {
                 userNumber /= DIVIDER ;     //otherwise it must have rest 1 and we should put it into binary array
-                binaryInterpretArray[i] = 1;
+                binaryInterpretArray[i] = ONE;
             }
 
         }
 
-//        while (userNumber > 0) {
-//            if (userNumber % DIVIDER == 0) {
-//                userNumber /= DIVIDER;
-//                System.out.print(" 0 ");
-//            } else {
-//                userNumber /= DIVIDER ;
-//                System.out.print(" 1 ");
-//            }
-
-
-//            System.out.println(userNumber);
-
-        for (int i = binaryInterpretArray.length - 1; i >= 0; i--) {
+        for (int i = binaryInterpretArray.length - ONE; i >= ZERO; i--) {
+            if ((i + ONE) % OUTPUT_SHOW_FORMAT == ZERO) {     //formating output into groups of 4
+                System.out.print(" ");
+            }
             System.out.print(binaryInterpretArray[i]);
         }
         }
