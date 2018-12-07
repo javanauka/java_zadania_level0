c1b5cc38b5e1bb1ae15a2d5aa603f8a05ceadd99
usuldib32 <usuldib32@gmail.com>
2018-12-07 11:06:15 +0100

    22 ready, all programs without 5 and 21 done

--- src/main/java/zadania_java_level0/Zadanie22.java
+++ src/main/java/zadania_java_level0/Zadanie22.java
@@ -1,10 +1,16 @@
 package zadania_java_level0;
 
-//create array of size n x n (n given by user, of boolean reange, vale 1 for cell if i+1 and j+1 are relatively prime (względnie pierwsze, nie wiem jak siętaczy) numbers
+//create array of size n x n (n given by user, of boolean range, value true for cell if i+1 and j+1 are relatively prime (względnie pierwsze, nie wiem jak to jest po angielsku) numbers
 
 import java.util.Scanner;
 
 public class Zadanie22 {
+
+    final static String CHAR_FOR_TRUE = "+";
+    final static String CHAR_FOR_FALSE = ".";
+    final static String TRUE = "true ";
+    final static String FALSE = "false ";
+
     public static void main(String[] args) {
         System.out.println("program z podanego n tworzy tablicę n x n, wypełnioną wartościami z zakresu boolean," +
                 "wartość komórki równa jest jeden dla a[i][j] gdy i+1 i j+1 są względnie pierwsze," +
@@ -17,76 +23,63 @@ public class Zadanie22 {
         rd.close();
 
         boolean[][] booleanArray = new boolean[arraySize][arraySize];
-        int[] check;
 
-        for (int i = 0; i < booleanArray.length; i++) {
+        for (int i = 0; i < booleanArray.length; i++) {     //creating array of boolean values
             for (int j = 0; j < booleanArray.length; j++) {
+                booleanArray[i][j] = IsRelativelyPrime(FindDividers(i + 1), j + 1);
+            }
+        }
 
-                booleanArray[i][j] = IsRelativelyPrime(FindDividers(i+1), j+1);
-                System.out.print((booleanArray[i][j]) + " ");
+        System.out.println("tablica podstawowa wygląda tak:");
+        PrintArray(booleanArray, CHAR_FOR_TRUE, CHAR_FOR_FALSE, 1);
+
+        System.out.println("tablica z określonym formatowaniem wygląda tak:");
+        PrintArray(booleanArray, CHAR_FOR_TRUE, CHAR_FOR_FALSE, 0);
+    }
+
+    public static int[] FindDividers(int numberToClculate) {    //method will find all possible dividers from 1 to numberToCalculate, inclusively
+        int[] arrayOfDividers = new int[numberToClculate];
+        int helperIndex = 0;        //not necessary but it was easier for me to check results
+        for (int i = 1; i <= numberToClculate; i++) {
+            if (numberToClculate % i == 0) {  //if modulo gives 0 it must be divider
+                arrayOfDividers[helperIndex++] = i;
+            }
+        }
+        return arrayOfDividers;
+    }
+
+    public static void PrintArray(boolean[][] array, String trueSign, String falseSign, int arrayTypeSwitch) {      //will show array with specifed sign or with true fale value
+        if (arrayTypeSwitch == 1) {
+            trueSign = TRUE;        //print with final signs
+            falseSign = FALSE;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i][j] == true) {
+                    System.out.print(trueSign);
+                } else {
+                    System.out.print(falseSign);
+                }
             }
             System.out.println();
         }
-        System.out.println(IsAPrimeNumber(1));
-
-        check = FindDividers(8);
-
-        PrintArray(check);
-
+        System.out.println();
     }
 
-
-public static boolean IsAPrimeNumber(int arrayIndex) {
-
-//    if (arrayIndex == 1) {
-//        return false;       //1 is not a prime number
-//    }
-
-    int dividerCount = 0;       //will help counting dividers of the value to examin if it prime number
-    for (int i = 2; i <= arrayIndex; i++) {
-        if (arrayIndex % i == 0) {
-            dividerCount++;
-        }
-    }
-    if (dividerCount == 0 ) {
-        return true;
-    }
-    return false;
-}
-
-public static int[] FindDividers(int numberToClculate) {
-        int[] arrayOfDividers = new int[numberToClculate];
-        int helperIndex = 0;
-    for (int i = 1; i <= numberToClculate; i++) {
-        if (numberToClculate % i == 0) {  //if modulo gives 0 it must be divider
-            arrayOfDividers[helperIndex++] = i;
-        }
-    }
-    return arrayOfDividers;
-}
-
-public static void PrintArray(int[] array) {
-    for(int i = 0; i <array.length;i++) {
-        System.out.print(array[i] + " ");
-    }
-    System.out.println();
-}
-
-public static boolean IsRelativelyPrime(int[] arrayOfDividers, int numberToCheck){
+    public static boolean IsRelativelyPrime(int[] arrayOfDividers, int numberToCheck) { //check if dividers for one number are unique  for it
         int commonDividersCounter = 0;
-        for(int i = 0; i < arrayOfDividers.length; i++){
+        for (int i = 0; i < arrayOfDividers.length; i++) {
             if (arrayOfDividers[i] > 0) {
                 if (numberToCheck % arrayOfDividers[i] == 0) {
                     commonDividersCounter++;
                 }
             }
         }
-        if (commonDividersCounter > 1) {
+        if (commonDividersCounter > 1) {    //if it have more than one divider numbers aren't relatively prime
             return false;
         }
         return true;
-}
-
+    }
 }
 
 
