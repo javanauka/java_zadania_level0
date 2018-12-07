64712c448c3495aae77bc2734df0be05faaf6667
usuldib32 <usuldib32@gmail.com>
2018-12-07 10:22:09 +0100

    22 works properly, shows boolean instead + .

--- src/main/java/zadania_java_level0/Zadanie22.java
+++ src/main/java/zadania_java_level0/Zadanie22.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 public class Zadanie22 {
     public static void main(String[] args) {
         System.out.println("program z podanego n tworzy tablicę n x n, wypełnioną wartościami z zakresu boolean," +
-                "wartość komórki jeden dla a[i][j] gdy i+1 i j+1 są pierwsze.");
+                "wartość komórki równa jest jeden dla a[i][j] gdy i+1 i j+1 są względnie pierwsze," +
+                " czyli dla obu największym wspólnym dzielnikiem jest 1.");
 
         System.out.println("podaj wymiartablicy n x n");
 
@@ -16,15 +17,22 @@ public class Zadanie22 {
         rd.close();
 
         boolean[][] booleanArray = new boolean[arraySize][arraySize];
+        int[] check;
 
         for (int i = 0; i < booleanArray.length; i++) {
             for (int j = 0; j < booleanArray.length; j++) {
 
+                booleanArray[i][j] = IsRelativelyPrime(FindDividers(i+1), j+1);
                 System.out.print((booleanArray[i][j]) + " ");
             }
             System.out.println();
         }
         System.out.println(IsAPrimeNumber(1));
+
+        check = FindDividers(8);
+
+        PrintArray(check);
+
     }
 
 
@@ -45,11 +53,40 @@ public static boolean IsAPrimeNumber(int arrayIndex) {
     }
     return false;
 }
-//wprowadzenie zman w zadaniu
-//a to dodałem po pullu
+
 public static int[] FindDividers(int numberToClculate) {
-        
-}
+        int[] arrayOfDividers = new int[numberToClculate];
+        int helperIndex = 0;
+    for (int i = 1; i <= numberToClculate; i++) {
+        if (numberToClculate % i == 0) {  //if modulo gives 0 it must be divider
+            arrayOfDividers[helperIndex++] = i;
+        }
+    }
+    return arrayOfDividers;
 }
 
-//co sie stanie jak tu cos wpisze i bede pullowac
+public static void PrintArray(int[] array) {
+    for(int i = 0; i <array.length;i++) {
+        System.out.print(array[i] + " ");
+    }
+    System.out.println();
+}
+
+public static boolean IsRelativelyPrime(int[] arrayOfDividers, int numberToCheck){
+        int commonDividersCounter = 0;
+        for(int i = 0; i < arrayOfDividers.length; i++){
+            if (arrayOfDividers[i] > 0) {
+                if (numberToCheck % arrayOfDividers[i] == 0) {
+                    commonDividersCounter++;
+                }
+            }
+        }
+        if (commonDividersCounter > 1) {
+            return false;
+        }
+        return true;
+}
+
+}
+
+
